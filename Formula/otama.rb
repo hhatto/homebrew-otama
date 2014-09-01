526af51f80d0d731204f3246a5c7c92abb0e9286
Hideo Hattori <hattori-h@klab.com>
2014-09-01 14:02:13 +0900

    update 0.7.3

--- Formula/otama.rb
+++ Formula/otama.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Otama < Formula
   homepage 'https://github.com/nagadomi/otama'
-  url 'https://github.com/nagadomi/otama/archive/v0.7.2.tar.gz'
-  sha1 '2da554957964c2c64dfd0e46e85c06c593577717'
+  url 'https://github.com/nagadomi/otama/archive/v0.7.3.tar.gz'
+  sha1 '92975cc623732963b0248d66bfe957d3895ae090'
 
   head 'https://github.com/nagadomi/otama.git'
 
