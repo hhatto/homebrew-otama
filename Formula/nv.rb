b91e09889f90b8eeceba7579c5925d6a28ad35e8
Hideo Hattori <hattori-h@klab.com>
2014-09-01 12:34:24 +0900

    update v2.2.0

--- Formula/nv.rb
+++ Formula/nv.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Nv < Formula
   homepage 'https://github.com/nagadomi/nv'
-  url 'https://github.com/nagadomi/nv/archive/v2.1.4.tar.gz'
-  sha1 'cd3d1cfebccf7b62ec220644cfe06cb14edc1d0b'
+  url 'https://github.com/nagadomi/nv/archive/v2.2.0.tar.gz'
+  sha1 'cd8746671cfa95d29ff61a4ea1ec8122375c939a'
 
   head 'https://github.com/nagadomi/nv.git'
 
